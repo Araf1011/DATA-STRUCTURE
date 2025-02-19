c707a1ed096f186f2200341e377355c8127db6a1
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2025-02-19 14:43:58 +0600

    Update NewNode.cpp

--- NewNode.cpp
+++ NewNode.cpp
@@ -1,43 +1,59 @@
-#incluude<iostream>
+#include <bits\stdc++.h>
 using namespace std;
 
-class node
-{
-public:
+
+class node{
+ public:
     int val;
-    node*next;
+    node* next;
     node(int value)
     {
-              this -> val =value;
-              this -> next =nullptr;
+        this->val = value;
+        this->next = nullptr;
     }
     node(node* next)
     {
-              this -> next = next;
+        this->next = next;
     }
 };
 class Linkedlist{
-public:
-          node*head;
-          node*tail;
-          Linkedlist()
-          {
-                    head = nullptr;
-                    tail = nullptr;
-          }
-          void insertInFront(int value)
-          {
-                    node * newnode =new node(value);
-                    id(head == null);
-          }
-          else{
-                    newnode -> next = head;
-                    head = newnode;
-          }
+  public:
+    node* head;
+    node* tail;
+    Linkedlist()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+    void insertInFront(int value)
+    {
+        node* newnode = new node(value);
+        if(head==NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            newnode->next = head;
+            head = newnode;
+        }
+    }
+
+
+
 };
+
+void solve()
+{
+    Linkedlist myList;
+    myList.insertInFront(5);
+    cout<<myList.head->val;
+}
 int main()
 {
 
+    cout<<"Here is the usage of Linked List: \n";
+    solve();
 
     return 0;
 }
